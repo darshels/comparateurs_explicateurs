b45d3f56c8355d671fb238660f98658e126427db
darshels <57956321+darshels@users.noreply.github.com>
2020-01-31 13:16:35 +0100

    Update comparateur.py

--- comparateur.py
+++ comparateur.py
@@ -1,4 +1,4 @@
-import comparateurs_explicateurs.evaluateurs.evaluateur_breakdown as bd
+#import comparateurs_explicateurs.evaluateurs.evaluateur_breakdown as bd
 import comparateurs_explicateurs.evaluateurs.evaluateur_ibreakdown as ibd
 import comparateurs_explicateurs.evaluateurs.evaluateur_lime as elime
 import comparateurs_explicateurs.evaluateurs.evaluateur_shap as eshap
@@ -8,7 +8,7 @@ import numpy as np
 def comparateur(data, y, feature_names, modele):
     np.set_printoptions(suppress=True)
     n_f = data.shape[1]
-    mauvaises_pred_bd = 0
+    #mauvaises_pred_bd = 0
     mauvaises_pred_ibd = 0
     mauvaises_pred_lime = 0
     mauvaises_pred_shap = 0
@@ -17,20 +17,20 @@ def comparateur(data, y, feature_names, modele):
     for i in range (1,n_f):
         print(i, "/", n_f)
         #Arbre, SVM, Random Forest, KNN, Logistic Regression, NN
-        mauvaises_pred_bd = mauvaises_pred_bd + bd.eval_bd(data, y, feature_names, i, modele)
+        #mauvaises_pred_bd = mauvaises_pred_bd + bd.eval_bd(data, y, feature_names, i, modele)
         mauvaises_pred_ibd = mauvaises_pred_ibd + ibd.eval_ibd(data, y, feature_names, i, modele)
         mauvaises_pred_lime = mauvaises_pred_lime + elime.eval_lime(data, y, feature_names, i, modele)
         mauvaises_pred_shap = mauvaises_pred_shap + eshap.eval_shap(data, y, feature_names, i, modele)
         
         
     #diviser la somme pour obtenir un taux moyen pour chaque algo pour ces données et ce modèle
-    mauvaises_pred_bd = mauvaises_pred_bd/(n_f-1)
+    #mauvaises_pred_bd = mauvaises_pred_bd/(n_f-1)
     mauvaises_pred_ibd = mauvaises_pred_ibd/(n_f-1)
     mauvaises_pred_lime = mauvaises_pred_lime/(n_f-1)
     mauvaises_pred_shap = mauvaises_pred_shap/(n_f-1)
     
     result = []
-    print("bd :", mauvaises_pred_bd)
+    #print("bd :", mauvaises_pred_bd)
     print("ibd :", mauvaises_pred_ibd)
     print("lime :", mauvaises_pred_lime)
     print("shap :", mauvaises_pred_shap)
